b2b730ef436f789ca1572b065132c3b75b1b6440
Jebb Burditt <jebb.burditt@gmail.com>
2025-10-15 20:53:35 -0700

    fix OnDestory bug

--- projects/fullswing-blog/src/app/blog/blog.ts
+++ projects/fullswing-blog/src/app/blog/blog.ts
@@ -1,10 +1,8 @@
-import { Component, computed, inject, input, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, input, OnInit } from '@angular/core';
 //import { DisqusModule } from 'ngx-disqus';
 import { MarkdownComponent } from 'ngx-markdown';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { BlogService } from './blog.service';
-import { HttpClient } from '@angular/common/http';
-import { Subscription } from 'rxjs';
 import { RepositoryService } from '../db/db';
 
 @Component({
@@ -12,13 +10,11 @@ import { RepositoryService } from '../db/db';
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
-export class Blog implements OnInit, OnDestroy {
+export class Blog implements OnInit {
   id = input.required();
   src = computed(() => `blog/${this.id()}.md`);
 
-  constructor(private http: HttpClient, private blogService: BlogService, private repositoryService: RepositoryService) {
-
-  }
+  constructor(private blogService: BlogService, private repositoryService: RepositoryService) { }
 
   ngOnInit(): void {
     const route = `/blog/${this.id()}`;
